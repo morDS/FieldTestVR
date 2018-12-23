f12cbee746bb782b444d34f4fe09dade296ef2a4
mordecai sayag <mor.sayag@gmail.com>
2018-12-23 10:57:05 +0200

    fresh start

---
+++ Assets/appMain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR;
+
+public class appMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRDevice.SetTrackingSpaceType(TrackingSpaceType.Stationary);
+        InputTracking.disablePositionalTracking = true;
+        InputTracking.Recenter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
